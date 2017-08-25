f48edf8094352713545b6d84a193d31c19f4a4e0
吕志杰 <lvzhijie@lvzhijiedeMacBook-Air.local>
2017-08-25 14:07:54 +0800

    啦啦啦

--- Today/ViewController.m
+++ Today/ViewController.m
@@ -20,7 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     my_block my = ^(NSString *str){
-        NSLog(@"%@%@哈哈哈我在好好考虑考虑",str,my);
+        NSLog(@"%@%@积极",str,my);
         
     };
     NSLog(@"success");
